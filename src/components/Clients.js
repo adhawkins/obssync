d4d2afc2aa798cec795a43857537d37ffc34accc
Andy Hawkins <andy@gently.org.uk>
2021-01-20 17:13:01 +0000

    Display column neadings

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -9,16 +9,25 @@ import {
 import OBSClient from './OBSClient.js';
 
 function Clients(props) {
-  const clientList = props.clients.length ? props.clients.map((item, key) =>
-          <OBSClient index={key}
-                key={key}
-                name={item.config.name}
-                address={item.config.address}
-                state={item.state}
-                onSceneClicked={props.changeSceneClicked}
-                onRecordingClicked={props.recordingClicked}
-                />
-    ) : (
+  const clientList = props.clients.length ? (
+        <React.Fragment>
+          <Row>
+            <Col className="font-weight-bold">Client</Col>
+            <Col className="font-weight-bold">Record Status</Col>
+            <Col className="font-weight-bold col-10">Scenes</Col>
+          </Row>
+          {props.clients.map((item, key) =>
+            <OBSClient index={key}
+                  key={key}
+                  name={item.config.name}
+                  address={item.config.address}
+                  state={item.state}
+                  onSceneClicked={props.changeSceneClicked}
+                  onRecordingClicked={props.recordingClicked}
+                  />
+          )}
+        </React.Fragment>
+        ) : (
       <Row>
         <Col>
           No clients
